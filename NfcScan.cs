49347adbcb4144034c0d8277b86717efbc3882da
Johan Jatko <johan@re3-labs.net>
2015-10-14 22:30:20 +0200

    Initial commit

---
+++ NfcScan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fail.hardware.NfcClient
+{
+    public class NfcScan
+    {
+        public NfcScanner Scanner;
+        public int CardId;
+    }
+}
---
+++ NfcScanner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace fail.hardware.NfcClient
+{ 
+    public class NfcScanner
+    {
+        static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        public string DeviceId { get; set; }
+        public string DeviceName { get; set; }
+        public string Endpoint { get; set; }
+
+
+        public static NfcScanner FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<NfcScanner>(json, SerializerSettings);
+        }
+    }
+
+}
